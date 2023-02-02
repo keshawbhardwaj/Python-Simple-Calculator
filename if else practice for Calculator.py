d092d190a79ef3c84292571dc2a5cf5876b42bc4
Keshaw Bhardwaj <119773619+keshawbhardwaj@users.noreply.github.com>
2023-02-02 22:44:42 +0530

    Add files via upload

---
+++ if else practice for Calculator.py
@@ -0,0 +1,16 @@
+#python proram for calculator
+input1 = int(input("enter 1st number: "))
+input2 = int(input("enter 2nd number: "))
+input3 = input('+,-,*,/ = ')
+if input3 == '+':
+    print(input1 + input2)
+elif input3 == '-':
+    print(input1-input2)
+elif input3  == '*':
+    print(input1*input2)
+elif input3=='/':
+    print(input1/input2)
+else:
+    print("Unexpected Error")
+
+
